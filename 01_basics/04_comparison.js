a7c2bd42fb910a1b9a21194c69739c9074d55066
Baqaullah <baqaullahdahani@gmail.com>
2025-07-12 21:37:24 +0000

    comparisions

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,23 @@
+// console.log(2>1)
+// console.log(2>=1)
+// console.log(2<1)
+// console.log(2==1)
+// console.log(2!=1)
+
+//console.log("02">1);
+
+// console.log(null>0)
+// console.log(null==0)
+// console.log(null>=0)
+
+
+// console.log(undefined>=0)
+// console.log(undefined>=0)
+// console.log(undefined>=0)
+
+//=== strict check, also check data typye
+
+// console.log("2"===1)
+
+// avoide comparing different data types, always compare similar data types
+//always keep your code clean
