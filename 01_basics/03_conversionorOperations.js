289be6a53e8fb7d262c9298f6c58f559085273da
Baqaullah <baqaullahdahani@gmail.com>
2025-07-12 20:43:27 +0000

    conversionorOperations

---
+++ 01_basics/03_conversionorOperations.js
@@ -0,0 +1,33 @@
+let score = false
+
+let valueInNumber = Number(score)
+//console.log(valueInNumber)
+
+// "33" => 33
+// "33abc" => NaN
+// True => 1; false => 0
+
+let isLoggedIn = ""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1 => true; 0=> false;
+// "" => false;
+// "Baqaullah" => true;
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber)
+
+let boolean = true
+let stringNum = String(boolean)
+
+console.log(typeof stringNum)
+
+let nullNum = null
+console.log(nullNum)
+
+let number = Number(null)
+console.log(typeof number)
