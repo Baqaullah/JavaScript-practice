069b67a9b2241d35a46ea945a1d2f8c31d2e6b6e
Baqaullah <baqaullahdahani@gmail.com>
2025-07-12 21:20:37 +0000

    operations

--- 01_basics/03_conversionorOperations.js
+++ 01_basics/03_conversionorOperations.js
@@ -18,16 +18,45 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber)
+//console.log(stringNumber);
+//console.log(typeof stringNumber)
 
 let boolean = true
 let stringNum = String(boolean)
 
-console.log(typeof stringNum)
+//console.log(typeof stringNum)
 
 let nullNum = null
-console.log(nullNum)
+//console.log(nullNum)
 
 let number = Number(null)
-console.log(typeof number)
+//console.log(typeof number)
+
+// *********************** Operations **************************//
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/2)
+// console.log(2%2)
+
+let str1 = "Hello"
+let str2 = " Baqaullah"
+let str3 = str1+str2
+// console.log(str3)
+
+// console.log(1+"2")
+// console.log(1+2+"2")
+
+let num1, num2, num3
+num1=num2=num3=2+2
+//console.log(num1)
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter)
