0450248d1c7cd8035c5447b901ae738e16c8c483
Baqaullah <baqaullahdahani@gmail.com>
2025-07-12 21:50:14 +0000

    datatype summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,5 @@
+//Primitive data types
+// 7 categories of primitive - String, , Number, Boolean, null, NaN, undefined, Symbol, BigInt
+
+// reference data type or non primitive data type
+// Arrays, Objects, Functions
