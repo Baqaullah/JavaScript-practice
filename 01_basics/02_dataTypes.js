c119ea0aee2baadadd698b8b7018d9ddd24ac0ee
Baqaullah <baqaullahdahani@gmail.com>
2025-07-12 19:18:27 +0000

    dataTypes changes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict"; //treat all JS code as newer version
+//alert(2+3) // we are use nodejs, not browser
+
+//console.log(2
+ //   +
+   // 3) // not do this, code readability should beb high
+
+   //Primitive data types
+   // number = 2 to power 53
+   // bigint 
+   // string => ""
+   //  boolean => true/ false
+   // null => standalone value
+   // undefined
+   // symbol = > unique
+
+   //Object
+   let age = 23;
+console.log(typeof undefined)
