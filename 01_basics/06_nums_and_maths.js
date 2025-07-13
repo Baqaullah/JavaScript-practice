94ff662eb5d6dd768813cbf4be225bdedf057e30
Baqaullah <baqaullahdahani@gmail.com>
2025-07-13 20:20:23 +0000

    numbers and maths

---
+++ 01_basics/06_nums_and_maths.js
@@ -0,0 +1,26 @@
+const score = 400
+// console.log(score)
+const balance = new Number(100.6700)
+// console.log(balance)
+// console.log(balance.toFixed(3))
+const otherNumber = 23.9876
+// console.log(otherNumber.toPrecision(4))
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString())
+
+//***********Maths*****************//
+
+// console.log(Math.abs(4))
+// console.log(Math.floor(4.4))
+// console.log(Math.min(4, 5, 6, 7, 2.4))
+// console.log(Math.max(4, 5, 6, 7, 2.4))
+
+
+console.log(Math.random())
+console.log(Math.floor(Math.random()*10)+1);
+
+
+const min = 10
+const max = 20
+console.log(Math.floor(Math.random()*(max - min +1)+min))
+
