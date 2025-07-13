988ad8bda27044f96242854632bc91793915dd31
Baqaullah <baqaullahdahani@gmail.com>
2025-07-13 14:17:47 +0000

    datatypes memory storate

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -3,3 +3,27 @@
 
 // reference data type or non primitive data type
 // Arrays, Objects, Functions
+
+
+///********************* Memory **************************//
+
+// Stack and Heap Memory
+// Stack (used in Primitive Data type) -- Heap (used in non-primitive data type)
+
+let myYoutubeName= "Study-Corner"
+let anotherName= myYoutubeName
+anotherName = "MyYoutubeChannel"
+
+console.log(myYoutubeName)
+console.log(anotherName)
+
+let userOne = {
+    email: "baqa@gmail.com",
+    addressCity: "Larkana"
+}
+
+let userTwo = userOne
+userTwo.email="studycorner@gmail.com"
+
+console.log(userOne.email)
+console.log(userTwo.email)
