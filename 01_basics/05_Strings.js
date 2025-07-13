22d146f9b01164835539d057a5f5d25ee6deb1c6
Baqaullah <baqaullahdahani@gmail.com>
2025-07-13 15:14:22 +0000

    string methods

---
+++ 01_basics/05_Strings.js
@@ -0,0 +1,37 @@
+let name = "Baqaullah"
+const repoCount = 50
+// console.log(name+repoCount+" Value")
+
+console.log(`Hello my name is ${name} and my repoCount is ${repoCount}`);
+
+gameName = new String('Baqa-ullah')
+
+// console.log(gameName.__proto__)
+// console.log(gameName.length)
+// console.log(gameName.charAt(8))
+// console.log(gameName.indexOf('u'))
+
+const newString = gameName.substring(0, 4)
+// console.log(newString)
+
+const anotherString = gameName.slice(-7, 4)
+
+// console.log(anotherString)
+
+const newStringOne ="      Baqa     "
+// console.log(newStringOne)
+// console.log(newStringOne.trim())
+
+const url = "https://baqaullahdahani.com/baqa%21dahani"
+// console.log(url.replace('%21', '-'))
+// console.log(url.includes('baloch'))
+
+// console.log(gameName.split('-'))
+
+let learn = "learn JavaScript with W3school"
+
+// console.log(learn.replace('W3school', 'Microsoft'))
+
+// console.log(learn.repeat(6))
+
+console.log(`My name is Baqaullah. I ${learn}.`)
