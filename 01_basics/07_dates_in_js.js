2dc864fb9f25b1ed1acc37e5ede0b6a1c808d77e
Baqaullah <baqaullahdahani@gmail.com>
2025-07-13 20:47:06 +0000

    Dates in JS

---
+++ 01_basics/07_dates_in_js.js
@@ -0,0 +1,16 @@
+// Dates
+
+// let myDate = new Date()
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleDateString());
+
+// let myCreatedDate = new Date(2023, 0, 23)
+// let myCreatedDate = new Date("2023-02-23")
+// let myCreatedDate = new Date("02-12-2023")
+// console.log(myCreatedDate.toLocaleString())
+// let myTimeStamp = Date.now()
+// console.log(myTimeStamp)
+
+let newDate = new Date()
+console.log(newDate.getDate()+1)
