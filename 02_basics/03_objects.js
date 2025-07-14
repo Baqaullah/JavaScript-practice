97e6550c668726eed7675819fd97d899d88c3f74
Baqaullah <baqaullahdahani@gmail.com>
2025-07-14 22:19:56 +0000

    objects

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,14 @@
+// singleton
+//object.create
+
+// object Literals
+
+const jsUser = {
+    name: "Baqaullah",
+    age: 30,
+    location: "Niger",
+    email: "baqa@gmail.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+console.log(jsUser.lastLoginDays)
