2987e1547d9df429378e63aead517d83619048b2
Baqaullah <baqaullahdahani@gmail.com>
2025-07-19 22:17:53 +0000

    control flow

---
+++ 04_control_flow/one.js
@@ -0,0 +1,12 @@
+// ////if
+// if(condition){
+//     //scope  -- if condition is true, code will execute ortherwise not
+// }
+
+const userLoggedIn = true
+const debitCard = true
+
+if (userLoggedIn && debitCard && 2==2){
+    console.log("You are allowed to make shopping")
+}
+
