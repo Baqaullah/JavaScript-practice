281b29ea57b38541199651bb006c35e8340faf48
Baqaullah <baqaullahdahani@gmail.com>
2025-07-20 18:19:38 +0000

    iterations

---
+++ 05_iterations/six.js
@@ -0,0 +1,41 @@
+const myNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const newNums = myNums.filter( (nums)=> nums > 6)
+
+const newNums = myNums.filter( (num)=>{
+    return num >4
+} )
+// console.log(newNums)
+
+const books = [
+    {
+        title: 'Book one', genere: 'Science', publish: '1981'
+    },
+    {
+        title: 'Book two', genere: 'Non Fiction', publish: '1992'
+    },
+    {
+        title: 'Book three', genere: 'history', publish: '1999'
+    },
+    {
+        title: 'Book four', genere: 'Non Fiction', publish: '1992'
+    },
+    {
+        title: 'Book five', genere: 'Science', publish: '2009'
+    },
+    {
+        title: 'Book six', genere: 'Fiction', publish: '1987'
+    },
+]
+
+// const userBooks = books.filter( (bk)=>bk.publish>1980)
+// const userBooks = books.filter( (bk)=>bk.genere ==="Science")
+// const userBooks = books.filter( (bk)=>bk.title ==="Book five")
+const userBooks = books.filter( (bk)=>bk.publish < 1990 && bk.genere === "Science")
+
+// console.log(userBooks)
+
+const myNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+const newNum = myNumbers.map( (num)=> num+10 )
+
+console.log(newNum)
