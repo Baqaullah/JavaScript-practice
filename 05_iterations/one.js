8cd8e809c217b6b96b52c82390b2d44897865397
Baqaullah <baqaullahdahani@gmail.com>
2025-07-20 12:22:29 +0000

    for loop

---
+++ 05_iterations/one.js
@@ -0,0 +1,39 @@
+// for loop
+// syntax  for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+// }
+
+for (let index = 0; index <= 10; index++) {
+    const element = index;
+    // console.log(element);
+    if (element == 8){
+        // console.log("8 is the best number")
+    }
+    // console.log(element)
+    
+}
+
+for (let i = 0; i <= 10; i++) {
+    // console.log(i)
+    for (let j = 0; j <=10; j++) {
+        // console.log(j)        
+        
+    }
+    
+}
+
+// for (let index = 1; index <= 20; index++) {
+//     if (index == 5){console.log("Detected 5")
+//     break}
+        
+//     console.log(`value of i is ${index}`)
+    
+// }
+for (let index = 1; index <= 20; index++) {
+    if (index == 5){console.log("Detected 5")
+    continue}
+        
+    console.log(`value of i is ${index}`)
+    
+}
